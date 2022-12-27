44a6734a35640390a09a50cf574c0f2f62bfc8b6
Craig Frayne <craig.frayne@aboutyou.com>
2022-12-27 15:12:44 +0100

    adding js nuggests

---
+++ js-nuggets/README.md
@@ -0,0 +1,6 @@
+## JS Nuggets
+### Coding Addict
+
+YouTube [playlist](https://www.youtube.com/playlist?list=PLnHJACx3NwAfRUcuKaYhZ6T5NRIpzgNGJ)
+
+
---
+++ screenshot-app/client/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Take a screenshot</h1>
+    </header>
+    <main>
+      <form @submit.prevent="requestScreenshot">
+        <input type="url" name="url" id="url" v-model="url" />
+        <input type="submit" value="Take screenshot">
+      </form>
+      <a v-if="screenshotUrl != ''" :href="screenshotUrl" target="_blank" download>
+        <img :src="screenshotUrl" />
+      </a>
+    </main>
+  </div>
+</template>
+
+<script>
+
+import { ref } from 'vue';
+
+export default {
+  name: 'App',
+  setup() {
+    const url = ref("http://");
+    const screenshotUrl = ref("");
+
+    const requestScreenshot = async () => {
+
+      console.log(url.value);
+/*       const res = await fetch('http://localhost:5000/screenshot', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          url: url.value
+        })
+      }).then(data => data.json)
+
+      console.log(res); */
+
+    }
+
+    return {
+      url,
+      screenshotUrl,
+      requestScreenshot
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
